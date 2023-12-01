0f0546a8663a75f8bed439e668ee0e536f8ab0e3
Gabor Makeev <gabor.makejev@gmail.com>
2023-12-01 19:54:51 +0100

    feat: adjust login to not create user entity in database; add user display name setting on sign up; add styling to the dashboard

--- pages/registration/registration.js
+++ pages/registration/registration.js
@@ -1,21 +1,20 @@
-import { createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-auth.js"
-import { collection, addDoc } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-firestore.js"
+import { createUserWithEmailAndPassword, updateProfile } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-auth.js"
 import { auth } from "../../firebase/auth.js"
-import { db } from "../../firebase/db.js"
 
 const registrationForm = document.getElementById("registration-form")
 registrationForm.addEventListener('submit',(e) => {
   e.preventDefault()
 
+  const name = document.getElementById("registration-form__name-input").value
   const email = document.getElementById("registration-form__email-input").value
   const password = document.getElementById("registration-form__password-input").value
 
-  addDoc(collection(db, "users"), {
-    email: email
-  })
-
   createUserWithEmailAndPassword(auth, email, password)
-    .then()
+    .then((userCredentials) => {
+      updateProfile(userCredentials.user, {
+        displayName: name
+      })
+    })
     .catch((error) => {
       console.log(error)
     })
