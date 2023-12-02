0832beabd81d903f0323993c325ea2974a92a3ef
Gabor Makeev <gabor.makejev@gmail.com>
2023-12-02 15:39:29 +0100

    feat: unify header styling; structure styles import; adjust index.js to use id instead of classes (html)

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const logInButton = document.getElementsByClassName('header__log-in-button')[0];
+const logInButton = document.getElementById('header__log-in-button');
 
 logInButton.onclick = () => {
   window.location.href = 'pages/login/login.html';
