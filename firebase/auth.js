9d971d8692ae634241c837b886605605f2df1eb7
Gabor Makeev <gabor.makejev@gmail.com>
2024-04-13 11:39:11 +0200

    initial laravel setup to replace the application with a laravel API app for the task manager

--- firebase/auth.js
+++
@@ -1,7 +0,0 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-app.js"
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-auth.js"
-import config from "./config.js"
-
-const firebaseConfig = config
-const app = initializeApp(firebaseConfig)
-export const auth = getAuth(app)
--- index.js
+++
@@ -1,6 +0,0 @@
-const logInButton = document.getElementById('header__log-in-button');
-
-logInButton.onclick = () => {
-  window.location.href = 'pages/login/login.html';
-}
-
