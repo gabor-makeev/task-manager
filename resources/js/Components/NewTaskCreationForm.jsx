80989a8ceaacf49535e8912abfcb8967133d5b55
Gabor Makeev <gabor.makejev@gmail.com>
2024-04-28 18:44:02 +0200

    implement quick task addition form

--- resources/js/Components/NewTaskCreationForm.jsx
+++ resources/js/Components/NewTaskCreationForm.jsx
@@ -1,7 +1,9 @@
-import {useRef, useState} from "react";
+import {useEffect, useRef, useState} from "react";
 
-export default function NewTaskCreationForm({ setShowingNewTaskCreationForm }) {
+export default function NewTaskCreationForm({ setShowingNewTaskCreationForm, tasks, setTasks }) {
     const [showingDescriptionInput, setShowingDescriptionInput] = useState(false)
+    const [taskNameInput, setTaskNameInput] = useState("")
+    const [taskDescriptionInput, setTaskDescriptionInput] = useState("")
 
     const overlay = useRef(null)
 
@@ -20,6 +22,12 @@ export default function NewTaskCreationForm({ setShowingNewTaskCreationForm }) {
     const handleSubmit = (e) => {
         e.preventDefault()
 
+        setTasks([{
+            id: tasks.length,
+            name: taskNameInput,
+            description: taskDescriptionInput
+        }, ...tasks])
+
         setShowingNewTaskCreationForm(false)
     }
 
@@ -31,14 +39,14 @@ export default function NewTaskCreationForm({ setShowingNewTaskCreationForm }) {
         <div id={"new-task-creation-form__overlay"} className={"absolute inset-0 bg-violet-950/10 flex justify-center items-start"} ref={overlay} onClick={(e) => handleOverlayClick(e)}>
             <form className={"flex flex-col bg-white rounded-xl shadow-sm max-w-2xl flex-grow mt-36"} onSubmit={(e) => handleSubmit(e)}>
                 <div className={"flex flex-col justify-center border-b px-6 h-12"}>
-                    <button className={"p-1.5 self-end hover:bg-gray-100 duration-150 rounded-lg"} onClick={(e) => handleCloseButtonClick(e)}><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="feather feather-x stroke-slate-500"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg></button>
+                    <button type={"button"} className={"p-1.5 self-end hover:bg-gray-100 duration-150 rounded-lg"} onClick={(e) => handleCloseButtonClick(e)}><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-x stroke-slate-500"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg></button>
                 </div>
                 <div className={"p-6 border-b"}>
-                    <input type="text" name={"task-name"} placeholder={"Task name"} className={"border-0 placeholder-slate-500 focus:ring-0 focus:outline-0 text-lg font-medium px-0 pt-0 pb-3"} />
+                    <input type="text" required autoFocus name={"task-name"} placeholder={"Task name"} onChange={(e) => setTaskNameInput(e.target.value)} className={"border-0 placeholder-slate-500 focus:ring-0 focus:outline-0 text-lg font-medium px-0 pt-0 pb-3 w-full"} />
                     <div className={"flex"}>
-                        {!showingDescriptionInput && <button onClick={handleAddDescriptionButtonClick} className={"group flex gap-2.5 text-slate-500 text-sm py-1.5 px-2 -mx-1.5 flex-grow hover:bg-gray-100 hover:text-slate-700 hover:rounded-md"}>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="feather feather-file stroke-slate-400 group-hover:stroke-slate-500"><path d="M13 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V9z"></path><polyline points="13 2 13 9 20 9"></polyline></svg>Add description</button>}
-                        {showingDescriptionInput && <textarea name="task-description" rows="10" placeholder={"Add description"} autoFocus className={"border-0 focus:ring-0 resize-none px-0 w-full"}></textarea>}
+                        {!showingDescriptionInput && <button onClick={handleAddDescriptionButtonClick} type={"button"} className={"group flex gap-2.5 text-slate-500 text-sm py-1.5 px-2 -mx-1.5 flex-grow hover:bg-gray-100 hover:text-slate-700 hover:rounded-md"}>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-file stroke-slate-400 group-hover:stroke-slate-500"><path d="M13 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V9z"></path><polyline points="13 2 13 9 20 9"></polyline></svg>Add description</button>}
+                        {showingDescriptionInput && <textarea name="task-description" rows="10" placeholder={"Add description"} onChange={(e) => setTaskDescriptionInput(e.target.value)} autoFocus className={"border-0 focus:ring-0 resize-none px-0 w-full"}></textarea>}
                     </div>
                 </div>
                 <div className="py-4 pr-4 pl-6 flex justify-end">
--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -1,26 +1,37 @@
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
 import { Head } from '@inertiajs/react';
-import {useState} from "react";
+import {useRef, useState} from "react";
 import NewTaskCreationForm from "@/Components/NewTaskCreationForm.jsx";
 
 export default function Dashboard({ auth }) {
     const [tasks, setTasks] = useState([]);
     const [taskNameInput, setTaskNameInput] = useState("")
     const [showingNewTaskCreationForm, setShowingNewTaskCreationForm] = useState(false)
+    const [showingQuickTaskCreationForm, setShowingQuickTaskCreationForm] = useState(false)
+    const [showingQuickTaskCreationFormControls, setShowingQuickTaskCreationFormControls] = useState(false)
 
-    const handleAddTask = (e) => {
-        e.preventDefault();
+    const quickTaskCreationFormSaveButton = useRef(null)
 
-        addTask(taskNameInput)
+    const handleQuickTaskCreationFormTaskNameInputBlur = (e) => {
+        if (e.relatedTarget === quickTaskCreationFormSaveButton.current) {
+            return
+        }
 
         setTaskNameInput("")
+        setShowingQuickTaskCreationFormControls(false)
+        setShowingQuickTaskCreationForm(false)
     }
 
-    const addTask = (taskName) => {
-        setTasks([...tasks, {
+    const handleQuickTaskCreationFormSubmit = (e) => {
+        e.preventDefault()
+
+        setTasks([{
             id: tasks.length,
-            name: taskName
-        }])
+            name: taskNameInput,
+        }, ...tasks])
+
+        setTaskNameInput("")
+        setShowingQuickTaskCreationForm(false)
     }
 
     return (
@@ -30,22 +41,33 @@ export default function Dashboard({ auth }) {
             setShowingNewTaskCreationForm={setShowingNewTaskCreationForm}
         >
             <Head title="Dashboard" />
-            {showingNewTaskCreationForm && <NewTaskCreationForm setShowingNewTaskCreationForm={setShowingNewTaskCreationForm} />}
+            {showingNewTaskCreationForm && <NewTaskCreationForm setShowingNewTaskCreationForm={setShowingNewTaskCreationForm} tasks={tasks} setTasks={setTasks} />}
 
             <div className="py-12">
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                    <form onSubmit={(e) => handleAddTask(e)}>
-                        <label htmlFor="taskName">Task name
-                            <input type="text" id={"taskName"} value={taskNameInput} onChange={(e) => {
-                                setTaskNameInput(e.target.value)
-                            }} />
-                        </label>
-                        <input type="submit" value="Add task" />
-                    </form>
-                    { tasks && <ul>
+                    <div className={"flex font-semibold items-center h-8"}>
+                        <h2 className={"mr-4 text-sm text-stone-800"}>Tasks</h2>
+                        <span className={"mr-2 text-xs text-slate-500"}>{ tasks.length }</span>
+                        {!showingQuickTaskCreationForm && <button onClick={() => setShowingQuickTaskCreationForm(true)} className={"flex items-center text-xs text-slate-500 hover:bg-gray-100 hover:rounded-md h-6 p-2"}>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-plus mr-1"><line x1="12" y1="5" x2="12" y2="19"></line><line x1="5" y1="12" x2="19" y2="12"></line></svg>Add Task</button>}
+                    </div>
+                    <div className={"text-xs text-slate-500 h-8 border-b border-b-slate-200 flex items-center"}>Name</div>
+                    <ul>
+                        {(!tasks.length || showingQuickTaskCreationForm) &&
+                            <li key={"quickTaskCreationForm"} className={"border-b border-b-slate-200"}>
+                                <form onSubmit={(e) => handleQuickTaskCreationFormSubmit(e)} className={"flex"}>
+                                    <label htmlFor="quickTaskCreationFormTaskNameInput" className={"hidden"}></label>
+                                    <input type="text" placeholder={"Task Name"} onFocus={() => setShowingQuickTaskCreationFormControls(true)} onBlur={(e) => handleQuickTaskCreationFormTaskNameInputBlur(e)} autoFocus required onChange={(e) => setTaskNameInput(e.target.value)} value={taskNameInput} id={"quickTaskCreationFormTaskNameInput"} className={"grow max-w-xl border-0 focus:ring-0 focus:outline-0 text-sm font-medium"} />
+                                    {showingQuickTaskCreationFormControls &&
+                                        <div className={"flex items-center"}>
+                                            <button ref={quickTaskCreationFormSaveButton} className={"flex items-center gap-1 px-2 bg-indigo-500 hover:bg-indigo-600 text-white rounded-md text-xs h-6"}>Save<svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="feather feather-corner-down-left"><polyline points="9 10 4 15 9 20"></polyline><path d="M20 4v7a4 4 0 0 1-4 4H4"></path></svg></button>
+                                        </div>
+                                    }
+                                </form>
+                            </li>
+                        }
                         {tasks.map(task => <li key={task.id}>{task.name}</li>)}
-                    </ul> }
-                    { !tasks.length && <p>There are no tasks yet created</p>}
+                    </ul>
                 </div>
             </div>
         </AuthenticatedLayout>
