df6605191e68022b565cb9d49cde1bb741cfd01b
Gabor Makeev <gabor.makejev@gmail.com>
2024-06-24 14:55:33 +0200

    create CloseTaskButton component

---
+++ resources/js/Components/Dashboard/TaskWindow/Components/Header/Components/CloseTaskButton/index.js
@@ -0,0 +1,3 @@
+import { CloseTaskButton } from "./CloseTaskButton.jsx"
+
+export default CloseTaskButton
--- resources/js/Components/Dashboard/TaskWindow/Components/Header/Header.jsx
+++ resources/js/Components/Dashboard/TaskWindow/Components/Header/Header.jsx
@@ -1,17 +1,11 @@
-import { router } from "@inertiajs/react"
 import SavingTaskDataIndicator from "./Components/SavingTaskDataIndicator"
 import TaskCreatedAtDate from "./Components/TaskCreatedAtDate"
 import CopyTaskIdButton from "./Components/CopyTaskIdButton"
 import VerticalSeparator from "./Components/VerticalSeparator"
 import DeleteTaskButton from "./Components/DeleteTaskButton"
+import CloseTaskButton from "./Components/CloseTaskButton"
 
 export const Header = ({ task, isTaskUpdating }) => {
-    const handleCloseButtonClick = (e) => {
-        e.preventDefault()
-
-        router.get(`/?${new URLSearchParams(window.location.search).toString()}`)
-    }
-
     return <>
         { isTaskUpdating && <SavingTaskDataIndicator />}
         <div className="border-b w-full h-12 flex justify-end items-center px-2">
@@ -20,9 +14,7 @@ export const Header = ({ task, isTaskUpdating }) => {
             <VerticalSeparator />
             <div className={"flex"}>
                 <DeleteTaskButton task={task} />
-                <button type={"button"} onClick={(e) => handleCloseButtonClick(e)} className={"duration-150 hover:bg-gray-100 rounded-lg w-8 h-8 flex items-center justify-center"}>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-x stroke-slate-500"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg>
-                </button>
+                <CloseTaskButton />
             </div>
         </div>
     </>
