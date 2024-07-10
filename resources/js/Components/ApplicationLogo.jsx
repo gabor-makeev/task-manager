b8ecbf2210196b6b7babeed179bc9593d9950c93
Gabor Makeev <gabor.makejev@gmail.com>
2024-07-10 21:21:28 +0200

    fix logo importing

--- resources/js/Components/ApplicationLogo.jsx
+++ resources/js/Components/ApplicationLogo.jsx
@@ -1,5 +1,5 @@
 export default function ApplicationLogo(props) {
 	return (
-		<img src="assets/logo.png" alt="logo of the application" {...props} />
+		<img src="/assets/logo.png" alt="logo of the application" {...props} />
 	)
 }
