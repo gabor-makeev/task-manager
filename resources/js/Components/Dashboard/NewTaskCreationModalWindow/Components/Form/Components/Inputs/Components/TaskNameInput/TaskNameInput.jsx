45e6106e3026ffcc8746ca88ab5abb6da83510d7
Gabor Makeev <gabor.makejev@gmail.com>
2024-09-22 20:29:31 +0200

    fix: fix the new task creation form's autofocus in case when there are no tasks in the list

--- resources/js/Components/Dashboard/NewTaskCreationModalWindow/Components/Form/Components/Inputs/Components/TaskNameInput/TaskNameInput.jsx
+++ resources/js/Components/Dashboard/NewTaskCreationModalWindow/Components/Form/Components/Inputs/Components/TaskNameInput/TaskNameInput.jsx
@@ -1,10 +1,18 @@
+import { useEffect, useRef } from "react"
+
 export const TaskNameInput = (props) => {
+	const inputRef = useRef(null)
+
+	useEffect(() => {
+		inputRef.current.focus()
+	}, [])
+
 	return (
 		<input
+			ref={inputRef}
 			{...props}
 			type="text"
 			required
-			autoFocus
 			name={"name"}
 			className={
 				"border-0 placeholder-slate-500 focus:ring-0 focus:outline-0 text-lg font-medium px-0 pt-0 pb-3 w-full"
