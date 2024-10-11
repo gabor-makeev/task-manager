6b33fc5f170eb3f3003456b7f362a96db94c6b75
Gabor Makeev <gabor.makejev@gmail.com>
2024-10-11 16:21:36 +0200

    feat: make default priorities to be created on user registration; implement relation between priorities and users (each user has their own set of priorities); remove not used seeders;

--- database/migrations/2024_05_26_124822_create_priorities_table.php
+++ database/migrations/2024_05_26_124822_create_priorities_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -16,6 +17,7 @@ return new class extends Migration
             $table->string('name')->unique();
             $table->tinyInteger('value')->unique();
             $table->enum('color', ['purple', 'blue', 'yellow', 'orange', 'red', 'gray', 'green'])->unique();
+            $table->foreignIdFor(User::class);
             $table->timestamps();
         });
     }
